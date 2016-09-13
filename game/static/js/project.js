dd88b2696c510aaf505faea6fa402675b2e734cc
Elaine Lin <elin1@mit.edu>
2016-09-12 22:30:03 -0400

    Styling navbar

--- game/static/js/project.js
+++ game/static/js/project.js
@@ -5,19 +5,20 @@
 }
 
 function countdown() {
-    var seconds = 60;
-    function tick() {
-        var counter = document.getElementById("counter");
-        seconds--;
-        counter.innerHTML = "0:" + (seconds < 10 ? "0" : "") + String(seconds);
-        if( seconds > 0 ) {
-            setTimeout(tick, 1000);
-        } else {
-            var submit = document.getElementById("submit");
-            submit.click();
-        }
+  var seconds = 60;
+  function tick() {
+    var counter = document.getElementById("counter");
+    seconds--;
+    counter.innerHTML = "0:" + (seconds < 10 ? "0" : "") + String(seconds);
+    if( seconds > 0 ) {
+        setTimeout(tick, 1000);
+    } else {
+      counter.style.color = "#F00";
+      var submit = document.getElementById("submit");
+      submit.click();
     }
-    tick();
+  }
+  tick();
 }
 
 countdown();
