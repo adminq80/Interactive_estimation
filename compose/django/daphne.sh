1874673b34b83a952740158fbbb8aed0f2123da1
Abdulrahman Alotaibi <adminq80@gmail.com>
2016-10-10 20:25:36 -0400

    Fixing the production code to work with runserver instead of a separate server

--- compose/django/daphne.sh
+++ compose/django/daphne.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
-python /app/manage.py collectstatic --noinput
-
-
-
-daphne config.asgi:channel_layer -b 0.0.0.0 -p 5000 --root-path=/app
-
-
-
-
+python /app/prod.py collectstatic --noinput
+#python /app/manage.py runworker -v2 --only-channels=http.* &
+#daphne -v2 config.asgi:channel_layer -b 0.0.0.0 -p 5000 --root-path=/app 
+python /app/prod.py runserver 0.0.0.0:5000
