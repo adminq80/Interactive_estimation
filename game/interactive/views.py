37c57c2b1ab4d07ddd6b5997ad308f0a1216821a
Abdulrahman Alotaibi <adminq80@gmail.com>
2016-10-12 21:58:59 -0400

    PEP 8

--- game/interactive/views.py
+++ game/interactive/views.py
@@ -16,10 +16,9 @@ from .models import Settings, Interactive, InteractiveRound
 
 
 def avatar(exclude=set()):
-    avatars = {'bee.png', 'bird.png', 'cat.png', 'cow.png', 'elephant.png', 'lion.png', 'pig.png',
-               'cute_cat.png','cute_panda.png','cute_giraffe.png','cute_owl.png',
-               'cute_cat_color.png',
-               'smart_dog.png','smart_fox.png','smart_monkey.png','smart_deer.png'
+    avatars = {'bee.png', 'bird.png', 'cat.png', 'cow.png', 'elephant.png', 'lion.png', 'pig.png', 'cute_cat.png',
+               'cute_panda.png', 'cute_giraffe.png', 'cute_owl.png', 'cute_cat_color.png', 'smart_dog.png',
+               'smart_fox.png', 'smart_monkey.png', 'smart_deer.png'
                }
     return choice(list(avatars.symmetric_difference(exclude)))
 
