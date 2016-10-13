92834188821055f6448a52056527225f5873dd78
amaatouq <amaatouq@gmail.com>
2016-10-12 21:09:42 -0400

    added avatars (cute and smart animals) and updated the code for selecting them

--- game/interactive/views.py
+++ game/interactive/views.py
@@ -16,7 +16,9 @@ from .models import Settings, Interactive, InteractiveRound
 
 
 def avatar(exclude=set()):
-    avatars = {'bee.png', 'bird.png', 'cat.png', 'cow.png', 'elephant.png', 'lion.png', 'pig.png'}
+    avatars = {'bee.png', 'bird.png', 'cat.png', 'cow.png', 'elephant.png', 'lion.png', 'pig.png',
+               'cute_cat.png','cute_panda.png','cute_giraffe.png','cute_owl.png','smart_dog.png','smart_fox.png','smart_monkey.png','smart_deer.png'
+               }
     return choice(list(avatars.symmetric_difference(exclude)))
 
 
