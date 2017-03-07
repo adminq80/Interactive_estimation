2580ee3fcbac651180fe7a58c4e0f4158deb2a2b
Abdulrahman Alotaibi <adminq80@gmail.com>
2017-03-07 14:05:18 -0500

    Added management commands for dynamic and static treatments

---
+++ game/interactive_static/management/commands/dump_static.py
@@ -0,0 +1,47 @@
+import json
+import datetime
+from decimal import Decimal
+
+from django.core.management.base import BaseCommand
+
+from game.interactive_static.models import InteractiveStatic, InteractiveStaticRound, Survey
+
+
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, Decimal):
+            return float(o)
+        if isinstance(o, datetime.datetime):
+            return str(o)
+        if isinstance(o, datetime.timedelta):
+            return o.seconds
+        return super(DecimalEncoder, self).default(o)
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        users = []
+
+        for game in InteractiveStatic.objects.all():
+            for u in game.users.all():
+                rounds = InteractiveStaticRound.objects.filter(user=u)
+                if rounds.count() < 1:
+                    continue
+                d = {'user': u.username,
+                     'final_score': u.get_score,
+                     'condition': 'control',
+                     'time_created': u.date_joined,
+                     'game_id': game.id,
+                     'unanswered': rounds.filter(guess__lt=0).count(),
+                     }
+                try:
+                    s = Survey.objects.get(username=u.username)
+                    survey = s.dump()
+                except Survey.DoesNotExist:
+                    survey = None
+                d['survey'] = survey
+                d['rounds'] = [r.round_data() for r in rounds]
+                # d['completed_hit'] = c.max_rounds == len(d['rounds'])
+                users.append(d)
+        print(json.dumps(users, cls=DecimalEncoder))
