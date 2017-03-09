9def7b32fbf9c6d7fb813ce75cd09d903928428e
Abdulrahman Alotaibi <adminq80@gmail.com>
2017-03-08 23:59:27 -0500

    Removed server hacks and used workers instead

--- compose/django/daphne.sh
+++ compose/django/daphne.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 python /app/prod.py collectstatic --noinput
-python manage.py rundelay &
-python /app/prod.py runserver 0.0.0.0:5000
-
-#python /app/manage.py runworker -v2 --only-channels=http.* &
-#daphne -v2 config.asgi:channel_layer -b 0.0.0.0 -p 5000 --root-path=/app
+daphne config.asgi:channel_layer -b 0.0.0.0 -p 5000
